b18c2753fb104fbf6bc146142ef5f47c58fdb5e4
LEEINAH <22na7592@gmail.com>
2025-02-24 12:16:38 +0900

    자동배포 테스트

--- src/main/java/com/koreait/short_url_project_02/domain/home/home/controller/HomeController.java
+++ src/main/java/com/koreait/short_url_project_02/domain/home/home/controller/HomeController.java
@@ -1,6 +1,5 @@
 package com.koreait.short_url_project_02.domain.home.home.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,22 +7,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
 
-    @Value("${custom.site.name}")
-    private String customSiteName;
-    //hi~
 
-    @Value("${custom.secret.key}")
     private String secretKey;
 
     @GetMapping("/")
     @ResponseBody
     public String home() {
-        return "Main Page!!! " + customSiteName;
+        return "Main Page 제발 띄워줄래??";
     }
 
-    @GetMapping("/secretKey")
-    @ResponseBody
-    public String secretKey() {
-        return "secretKey :  " + secretKey;
-    }
 }
